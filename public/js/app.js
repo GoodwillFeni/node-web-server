fcc0ceeb701a615376b4dcb2e51cae4a40b8389b
GoodwillFeni <69680960+GoodwillFeni@users.noreply.github.com>
2022-01-02 12:23:35 +0200

    Added space to test on commit

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-   //FETCH THE WEATHER
+    //FETCH THE WEATHER
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
